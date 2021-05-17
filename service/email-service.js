f5e816dc605f0f688c879a8613afa2b41a7cd5b0
Rukshan Jayasekara <rukshanjayasekara@outlook.com>
2021-05-17 22:52:14 +0530

    E-mail service update

--- service/email-service.js
+++ service/email-service.js
@@ -1,36 +1,31 @@
 const nodemailer = require("nodemailer");
 
+require('dotenv').config({path: __dirname + '/.env'})
+
+const GMAIL_USERNAME = process.env['GMAIL_USERNAME'];
+const GMAIL_PASSWORD = process.env['GMAIL_PASSWORD'];
+
 async function MailService() {
-  
-    // Generate test SMTP service account from ethereal.email for testing purposes
-  let testAccount = await nodemailer.createTestAccount();
 
   // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.ethereal.email",
+    host: "smtp.gmail.com",//"smtp.ethereal.email",
     port: 587,
-    secure: false, // true for 465, false for other ports
+    secure: false,
     auth: {
-      user: testAccount.user, // generated ethereal user
-      pass: testAccount.pass, // generated ethereal password
+      user: GMAIL_USERNAME,
+      pass: GMAIL_PASSWORD
     },
   });
 
-  // send mail with defined transport object
   let info = await transporter.sendMail({
-    from: '"Fred Foo 👻" <foo@example.com>', // sender address
-    to: "bar@example.com, baz@example.com", // list of receivers
-    subject: "Hello ✔", // Subject line
-    text: "Hello world?", // plain text body
-    html: "<b>Hello world?</b>", // html body
+    from: '"The Gadget Store 👻" <gadgets@store.com>',
+    to: "rukshanjayasekara@outlook.com",
+    subject: "Payment Successful ✔",
+    text: "Thank you! Visit us again.",
+    html: "<b>Thank you! Visit us again.</b>",
   });
 
-  console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
 MailService().catch(console.error);
